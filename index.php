72b3609324ef61d1fec498762feea81403580114
ingmarhendrik <68082792+ingmarhendrik@users.noreply.github.com>
2023-10-24 13:29:09 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!--Ülesanne 1. Ingmar Hendrik Rohusaar. 10.oktoober-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <?php
+        // 1.
+        $name = "Ingmar";
+        $birth_year = 2005;
+        $zodiac_sign = "Gemini";
+        
+        echo "My name is \n$name\nand I was born in \n$birth_year.\nMy zodiac sign is \n$zodiac_sign.";
+
+        // 2.
+        echo "\n\"It's My Life\" - Dr. Alban";
+        
+        // 3. 
+        echo "\n(\(\\\n( -.-)\no_('')('')"
+    ?>
+
+
+</body>
+</html>
